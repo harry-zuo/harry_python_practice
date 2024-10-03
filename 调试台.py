c6b47f50563fe174f2cc1e3a61ddea92c897801f
mengjielee <limengjiejj@hotmail.com>
2024-10-03 23:34:52 +0800

    add 20241003 update stuff

--- 调试台.py
+++ 调试台.py
@@ -1,22 +1,45 @@
 """
-输入：s = "  hello world  "
-输出："world hello"
-解释：反转后的字符串中不能存在前导空格和尾随空格。
+输入：dist = [1,3,2], hour = 1.9
+输出：-1
+
+输入：dist = [1,3,2], hour = 2.7
+输出：3
 """
 
-s = "a good   example"
+dist = [1,3,2]
+hour = 6
 
-def func(s):
-    s = s.strip()
-    left_p = right_p = len(s) - 1
-    answer = []
-    while left_p >= 0:
-        while left_p >=0 and s[left_p] != " ": left_p -= 1
-        answer.append(s[left_p+1:right_p+1])
-        while s[left_p] == " ": left_p -= 1
-        right_p = left_p
+def func(dist: list[int], hour: float) -> int:
+    n = len(dist)
+    h100 = round(hour * 100)
+    delta = h100 - (n - 1) * 100
+    if delta <= 0:
+        return -1
 
-    return " ".join(answer)
+    max_dist = max(dist)
+    if n * 100 >= h100:
+        return max(max_dist, (dist[-1]*100 - 1)//delta + 1)
+
+    def check(v: int) -> bool:
+        t = n - 1
+        for d in dist[:-1]:
+            t += d - 1 // v
+        return (t * v + dist[-1]) * 100 <= h100 * v
+
+    left = 0
+    h = h100 // (n * 100)
+    right = (max_dist - 1) // h + 1
+    while left + 1 < right:
+        mid = (left + right) // 2
+        if check(mid):
+            right = mid
+        else:
+            left = mid
+    return right
         
-res = func(s)
+    return speed
+        
+res = func(dist, hour)
 print(res)
+
+
